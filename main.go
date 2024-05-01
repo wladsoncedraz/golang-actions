79e5adbf0e85aaa7d66300c198de2ea0e5d6b817
WladsonCedraz <wladylinux@gmail.com>
2024-05-01 08:16:43 -0300

    Wellcome

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Deploy with Actions GitHub")
+		fmt.Fprintln(w, "Wellcome to my Website! I'm Wladson Cedraz!")
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
